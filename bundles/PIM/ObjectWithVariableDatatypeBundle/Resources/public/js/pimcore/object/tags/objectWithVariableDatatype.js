2cf7cdbd81cfd33428ef57bd1f021c475ca5cc52
javra-rojan <rojan.karki@javra.com>
2022-07-07 18:23:49 +0545

    Error on object with var data type

--- bundles/PIM/ObjectWithVariableDatatypeBundle/Resources/public/js/pimcore/object/tags/objectWithVariableDatatype.js
+++ bundles/PIM/ObjectWithVariableDatatypeBundle/Resources/public/js/pimcore/object/tags/objectWithVariableDatatype.js
@@ -3,23 +3,23 @@ pimcore.object.tags.objectWithVariableDatatype = Class.create( pimcore.object.ta
     type: "objectWithVariableDatatype",
 
     getEditToolbarItems: function (){
-        var toolbarItems = [
-            {
-                xtype: "tbspacer",
-                width: 20,
-                height: 20,
-                cls: "pimcore_icon_droptarget"
-            },
-            {
-                xtype: "tbtext" ,
-                text: "<b>" +this.fieldConfig.title+ "<br>"
-            },
-            {
-                xtype: "button",
-                iconCls: "pimcore_icon_delete",
-                handler: this.empty.bind(this)
-            }
-        ]
-        return toolbarItems;
+        // var toolbarItems = [
+        //     {
+        //         xtype: "tbspacer",
+        //         width: 20,
+        //         height: 20,
+        //         cls: "pimcore_icon_droptarget"
+        //     },
+        //     {
+        //         xtype: "tbtext" ,
+        //         text: "<b>" +this.fieldConfig.title+ "<br>"
+        //     },
+        //     {
+        //         xtype: "button",
+        //         iconCls: "pimcore_icon_delete",
+        //         handler: this.empty.bind(this)
+        //     }
+        // ]
+        // return toolbarItems;
     }
 });
